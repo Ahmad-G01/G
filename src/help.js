e38f27c19a58e19631ae2feecb54c07c538e2387
CONTOH254 <76092829+CONTOH254@users.noreply.github.com>
2020-12-28 06:20:54 +0700

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,64 @@
+const help = (prefix) => {
+	return `
+ INFORMATION 
+
+ GUSTIAN BOT
+ CREATOR : *AHMAD*
+ wa.me/6282137409151
+
+ LIST MENU 
+
+ *${prefix}stiker* 
+ *${prefix}stikergif*
+ *${prefix}wait*
+ *${prefix}toimg*
+ *${prefix}tsticker*
+ *${prefix}meme*
+ *${prefix}gtts [kode] [teks]*
+ *${prefix}memeindo*
+ *${prefix}loli*
+ *${prefix}nsfwloli*
+ *${prefix}simi*
+ *${prefix}ocr*
+ *${prefix}stickernobg*
+ *${prefix}info*
+ *${prefix}tiktok*
+ *${prefix}fitnah*
+ *${prefix}tiktokstalk*
+ *${prefix}blocklist*
+
+    *play* [judul lagu]
+ ADMIN GROUP 
+
+ *${prefix}promote* [tag]
+ *${prefix}demote* [tag]
+ *${prefix}kick* [tag]
+ *${prefix}tagall*
+ *${prefix}welcome* 1/0
+ *${prefix}simih* 1/0
+ *${prefix}add* [62xxx]
+ *${prefix}nsfw* [1/0]
+
+ SPESIAL 
+
+ *#owner*
+ *#donasi*
+ *#databot*
+ *#salam*
+ *#goblok*
+ *#anjir*
+
+ OWNER 
+
+ *${prefix}setprefix*
+ *${prefix}bc*
+ *${prefix}clone*
+ *${prefix}leave*
+ *${prefix}block*
+ *${prefix}unblock*
+ *${prefix}clearall*
+
+ GUSTIAN BOT 
+}
+
+exports.help = help
